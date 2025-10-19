f7de075a8cfe2243643174102422c352d383f44f
esraa237 <esraafoda2000@gmail.com>
2025-10-20 00:05:24 +0300

    edit something related to files

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,8 @@ import { SeederModule } from './seeder/seeder.module';
 import { FilesModule } from './files/file.module';
 import { ConfigurationModule } from './configuration/configuration.module';
 import { PagesModule } from './pages/pages.module';
-import { join } from 'path';
+import { extname, join } from 'path';
+import * as mime from 'mime-types';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { FILE_CONSTANTS } from './files/contstants/file.constant';
 import { ServicesModule } from './services/services.module';
@@ -31,7 +32,14 @@ import { ApplicationsModule } from './applications/applications.module';
       rootPath: join(__dirname, '..', FILE_CONSTANTS.UPLOAD_DIR),
       serveRoot: `/${FILE_CONSTANTS.UPLOAD_DIR}`,
       serveStaticOptions: {
-        index: false, 
+        index: false,
+        setHeaders: (res, path) => {
+          const contentType = mime.lookup(extname(path));
+          if (contentType) {
+            res.setHeader('Content-Type', contentType);
+          }
+          res.setHeader('Content-Disposition', 'inline');
+        },
       },
     }),
     ServicesModule,
